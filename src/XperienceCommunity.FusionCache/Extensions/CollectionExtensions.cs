548a554a7367a24d96e210199dcdcb9109d75ae2
Duane Gibbs <duane.gibbs@crafted.co.uk>
2025-01-21 12:53:51 +0000

    Allow for custom vary by options during [OutputCache] setup

---
+++ src/XperienceCommunity.FusionCache/Extensions/CollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace XperienceCommunity.FusionCache.Extensions;
+
+/// <summary>
+/// Collection extension methods.
+/// </summary>
+internal static class CollectionExtensions
+{
+    /// <summary>
+    /// Adds one dictionary to another.
+    /// </summary>
+    /// <typeparam name="TKey">Key type.</typeparam>
+    /// <typeparam name="TValue">Value type.</typeparam>
+    /// <param name="source">Source dictionary.</param>
+    /// <param name="other">Dictionary to add.</param>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static void AddRange<TKey, TValue>(this IDictionary<TKey, TValue> source, IDictionary<TKey, TValue> other)
+    {
+        if (other is null)
+        {
+            throw new ArgumentNullException(nameof(other));
+        }
+
+        foreach (var item in other)
+        {
+            source.TryAdd(item.Key, item.Value);
+        }
+    }
+}
--- src/XperienceCommunity.FusionCache/Services/CacheVaryByOptionService.cs
+++ src/XperienceCommunity.FusionCache/Services/CacheVaryByOptionService.cs
@@ -37,9 +37,47 @@ public class CacheVaryByOptionService
             yield break;
         }
 
-        foreach (var type in types)
+        foreach (var type in types.OrderBy(x => x.FullName))
         {
             yield return (ICacheVaryByOption)ActivatorUtilities.CreateInstance(services, type);
         }
+    }
+
+    /// <summary>
+    /// Gets a <see cref="Dictionary{TKey, TValue}"/> for the provided vary by option types.
+    /// </summary>
+    /// <param name="types">Vary by types.</param>
+    /// <returns><see cref="Dictionary{TKey, TValue}"/>.</returns>
+    public IDictionary<string, string>? GetVaryByOptionsDictionary(Type[]? types)
+    {
+        if (types is null)
+        {
+            return null;
+        }
+
+        var services = httpContextAccessor?.HttpContext?.RequestServices;
+
+        if (services is null)
+        {
+            return null;
+        }
+
+        var dict = new Dictionary<string, string>();
+
+        foreach (var type in types)
+        {
+            var varyByOption = (ICacheVaryByOption)ActivatorUtilities.CreateInstance(services, type);
+
+            string? keyValue = varyByOption?.GetKey();
+
+            if (string.IsNullOrEmpty(keyValue))
+            {
+                continue;
+            }
+
+            dict.TryAdd(type.Name, keyValue);
+        }
+
+        return dict;
     }
 }
